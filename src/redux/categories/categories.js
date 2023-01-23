9099f47240fb70ff7d76d04ac0df11829a355ac7
shegy28 <shegy28@gmail.com>
2023-01-23 22:02:14 +0100

    Fix linter error

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,17 +1,15 @@
 const STATUS_CHECKED = 'bookstore/categories/STATUS_CHECKED';
 
-const checkStatusAction = () => {
-    return {
-        type: STATUS_CHECKED,
-    };
-}
+const checkStatusAction = () => ({
+  type: STATUS_CHECKED,
+});
 
 const categoryReducer = (state = [], action = {}) => {
-    switch(action.type){
-        case STATUS_CHECKED: return "Under construction";
-        default: return state;
-    }
-}
+  switch (action.type) {
+    case STATUS_CHECKED: return 'Under construction';
+    default: return state;
+  }
+};
 
-export {checkStatusAction};
+export { checkStatusAction };
 export default categoryReducer;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,10 @@ import bookreducer from './books/books';
 import categoryReducer from './categories/categories';
 
 const store = configureStore({
-    reducer:{
-        book: bookreducer,
-        category: categoryReducer
-    },
-})
+  reducer: {
+    book: bookreducer,
+    category: categoryReducer,
+  },
+});
 
 export default store;
