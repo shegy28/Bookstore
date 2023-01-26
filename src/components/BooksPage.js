93aa1157b95c0f219381c9c5119f319fed8792f8
shegy28 <shegy28@gmail.com>
2023-01-26 13:41:46 +0100

    Add useEffect to fetch api on page load

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import  { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getBookAction } from '../redux/books/books';
 import AddBook from './AddBook';
 import BookLists from './BookLists';
 
 const BooksPage = () => {
   const books = useSelector((state) => state.book);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBookAction());
+  }, [dispatch]);
 
   return (
     <div>
